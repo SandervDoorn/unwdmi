7f38a55b02a70d3d15bff9256ac8af59cbd8f57f
Sander van Doorn <s.van.doorn@st.hanze.nl>
2019-01-23 14:18:01 +0100

    Code cleanup, secure file opening and better quotation

--- jsonparser.py
+++ jsonparser.py
@@ -15,11 +15,8 @@ file.close()
 path = "json-testfiles/" + j["station"]
 permissions = 0o755
 if not os.path.isdir(path):
-    print("Folder doesnt exist")
-    print("Creating folder...")
     os.mkdir(path, permissions)
 
-print("Folder found, loading data...")
 measurement = j["measurement"]
 csvpath = path + "/" + measurement["date"] + ".csv"
 newfile = True
@@ -27,11 +24,10 @@ newfile = True
 if os.path.exists(path + "/" + measurement["date"] + ".csv"):
     newfile = False
 
-with open(csvpath, "a") as csvfile:
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+with open(csvpath, "a", newline='') as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
     if newfile:
         writer.writerow(['time', 'date', 'temperature', 'humidity', 'dewpoint'])
-    # TODO: writerow overwrites if file exists, change to add a line instead
     writer.writerow([measurement['time'],
                      measurement['date'],
                      measurement['temperature'],
