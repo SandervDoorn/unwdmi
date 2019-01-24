cbc2aa2f7552069c453b782ebf496ff1dfaf8787
Martin Vinke <martin.vinke.mv@gmail.com>
2019-01-24 12:03:07 +0100

    andere methode

--- src/Threading/HandleRequestThread.java
+++ src/Threading/HandleRequestThread.java
@@ -5,50 +5,23 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class HandleRequestThread implements Runnable {
-    private BufferedReader in;
-    private Socket socket;
-    private LinkedBlockingQueue<JSONObject> queue;
+    private StringBuilder xml;
 
     /**
-     * @param clientSocket
-     * @param XMLQueue
+     * @param xml
      * @throws IOException
      */
-    public HandleRequestThread(Socket clientSocket, LinkedBlockingQueue<JSONObject> XMLQueue) throws IOException {
-        this.socket = clientSocket;
-        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        this.queue = XMLQueue;
+    public HandleRequestThread(StringBuilder xml) throws IOException {
+        this.xml = xml;
     }
 
     @Override
     public void run() {
-        StringBuilder XMLElement = new StringBuilder();
-        //TODO remove the test variable when done
-        int test = 0;
+        XMLParser parser = new XMLParser(this.xml);
+        JSONObject parsedJson = parser.parseXML();
 
-        try {
-            String xmlLine;
-            while (this.socket.isConnected() && test < 10) {
-                xmlLine = this.in.readLine();
-                XMLElement.append(xmlLine);
-                if (xmlLine.equals("</WEATHERDATA>")) {
-                    XMLParser XMLParser = new XMLParser(XMLElement.toString());
-
-                    //end xml element and add it to queue
-                    queue.put(XMLParser.parseXML());
-                    XMLElement = new StringBuilder();
-                    test++;
-                }
-            }
-            System.out.println(queue);
-            System.exit(1);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/Threading/RealMultiThreaded.java
+++ src/Threading/RealMultiThreaded.java
@@ -1,9 +1,13 @@
 package Threading;
 
+import Parsing.XMLParser;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.*;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -14,18 +18,33 @@ public class RealMultiThreaded {
     /**
      * @param args
      */
-    public static void main (String[] args)
+    public static void main (String[] args) throws Exception
     {
-        LinkedBlockingQueue<JSONObject> XMLQueue = new LinkedBlockingQueue<>();
+        LinkedBlockingQueue<StringBuilder> XMLQueue = new LinkedBlockingQueue<>();
 
         try (
-                ServerSocket serverSocket = new ServerSocket(7789)
+                ServerSocket serverSocket = new ServerSocket(7789);
+                Socket       clientSocket = serverSocket.accept();
+                BufferedReader in  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         ) {
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                threadPool.execute(new HandleRequestThread(clientSocket, XMLQueue));
-            }
+            String xmlLine;
+            StringBuilder XMLElement = new StringBuilder();
 
+            //luister naar </weatherdata>
+            //flikker dat in een queue
+            while (true) {
+                xmlLine = in.readLine();
+                System.out.println(xmlLine);
+                XMLElement.append(xmlLine);
+                if (xmlLine.equals("</WEATHERDATA>")) {
+                    //end xml element and add it to queue
+                    XMLQueue.put(XMLElement);
+                    XMLElement = new StringBuilder();
+                    //maak threads aan die shit uit de queue halen en verwerkemn
+
+                    threadPool.execute(new HandleRequestThread(XMLQueue.take()));
+                }
+            }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
 
