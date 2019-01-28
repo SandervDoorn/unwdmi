54e83507e5dbf8e74b42424a0505a032d0a0e524
Sander van Doorn <s.van.doorn@st.hanze.nl>
2019-01-28 11:35:41 +0100

    Added certificates to support SSL Socket now wrapped in SSL

--- storageserver/client.py
+++ storageserver/client.py
@@ -1,4 +1,5 @@
 from socket import *
+import ssl
 import configparser
 
 config = configparser.ConfigParser()
@@ -6,50 +7,53 @@ config.read("config.ini")
 
 HOST = config['lacthosa']['host']
 PORT = int(config['lacthosa']['port'])
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.load_verify_locations('certificates/lacthosa.crt')
 
 with socket(AF_INET, SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    examplejson = """[
-    {
-        "station": 123456,
-        "measurement": {
-            "time": "15:01",
-            "date": "21-01-19",
-            "temperature": "21",
-            "humidity": "10",
-            "dewpoint": "15"
+    with context.wrap_socket(s, server_hostname=HOST) as sock:
+        sock.connect((HOST, PORT))
+        examplejson = """[
+        {
+            "station": 123456,
+            "measurement": {
+                "time": "15:01",
+                "date": "21-01-19",
+                "temperature": "21",
+                "humidity": "10",
+                "dewpoint": "15"
+            }
+        },
+        {
+            "station": 123457,
+            "measurement": {
+                "time": "15:01",
+                "date": "21-01-19",
+                "temperature": "21",
+                "humidity": "10",
+                "dewpoint": "15"
+            }
+        },
+        {
+            "station": 123458,
+            "measurement": {
+                "time": "15:01",
+                "date": "21-01-19",
+                "temperature": "21",
+                "humidity": "10",
+                "dewpoint": "15"
+            }
+        },
+        {
+            "station": 123459,
+            "measurement": {
+                "time": "15:01",
+                "date": "21-01-19",
+                "temperature": "21",
+                "humidity": "10",
+                "dewpoint": "15"
+            }
         }
-    },
-    {
-        "station": 123457,
-        "measurement": {
-            "time": "15:01",
-            "date": "21-01-19",
-            "temperature": "21",
-            "humidity": "10",
-            "dewpoint": "15"
-        }
-    },
-    {
-        "station": 123458,
-        "measurement": {
-            "time": "15:01",
-            "date": "21-01-19",
-            "temperature": "21",
-            "humidity": "10",
-            "dewpoint": "15"
-        }
-    },
-    {
-        "station": 123459,
-        "measurement": {
-            "time": "15:01",
-            "date": "21-01-19",
-            "temperature": "21",
-            "humidity": "10",
-            "dewpoint": "15"
-        }
-    }
-    ]"""
-    s.sendall(examplejson.encode())
+        ]"""
+        sock.sendall(examplejson.encode())
 
--- storageserver/server.py
+++ storageserver/server.py
@@ -1,4 +1,5 @@
 from socket import *
+import ssl
 from storageserver import jsonparser
 import configparser
 
@@ -7,20 +8,24 @@ config.read("config.ini")
 
 HOST = config['lacthosa']['host']
 PORT = int(config['lacthosa']['port'])
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.load_cert_chain('certificates/lacthosa.crt', 'certificates/lacthosa.key')
+
 
 while True:
     with socket(AF_INET, SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen(1)
-        try:
-            conn, addr = s.accept()
-            print("Connection established...")
-            with conn:
-                while True:
-                    data = conn.recv(2048)
-                    if not data:
-                        break
-                    jsonparser.from_stream(data.decode())
-        except ConnectionResetError:
-            print("Connection terminated unexpectedly, closing socket...")
-            s.close()
+        with context.wrap_socket(s, server_side=True) as sock:
+            try:
+                conn, addr = sock.accept()
+                print("Connection established...")
+                with conn:
+                    while True:
+                        data = conn.recv(2048)
+                        if not data:
+                            break
+                        jsonparser.from_stream(data.decode())
+            except ConnectionResetError:
+                print("Connection terminated unexpectedly, closing socket...")
+                sock.close()
