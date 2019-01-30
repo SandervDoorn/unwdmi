e7a9f0922d7aaf02173d613d7be96c9e8cdcf467
Sander van Doorn <s.van.doorn@st.hanze.nl>
2019-01-30 12:39:30 +0100

    Old files are deleted upon successful calculations

--- storageserver/cleanhistory.py
+++ storageserver/cleanhistory.py
@@ -18,6 +18,7 @@ stationFolders = subdirs[0]
 
 # For each station directory:
 for statdir in stationFolders:
+    successFlag = True
     file = rootdir + statdir + '/' + dayToAverage + '.csv'
     avgfile = rootdir + statdir + '/averages.csv'
     if os.path.exists(file):
@@ -25,22 +26,29 @@ for statdir in stationFolders:
         hums = []
         dewps = []
 
-        # Read yesterdays csv files and store all data in lists
-        with open(file) as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                temps.append(float(row['temperature']))
-                hums.append(float(row['humidity']))
-                dewps.append(float(row['dewpoint']))
+        try:
+            # Read yesterdays csv files and store all data in lists
+            with open(file) as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    temps.append(float(row['temperature']))
+                    hums.append(float(row['humidity']))
+                    dewps.append(float(row['dewpoint']))
 
-        # Check if avg file exists or create
-        if not os.path.exists(avgfile):
-            with open(avgfile, 'a', newline='') as csvfile:
+            # Check if avg file exists or create
+            if not os.path.exists(avgfile):
+                with open(avgfile, 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+                    writer.writerow(['date', 'temperature', 'humidity', 'dewpoint'])
+
+            # Open avg file and add averages
+            with open(avgfile, 'a') as csvfile:
                 writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-                writer.writerow(['date', 'temperature', 'humidity', 'dewpoint'])
+                writer.writerow([dayToAverage, sum(temps)/len(temps), sum(hums)/len(hums), sum(dewps)/len(dewps)])
 
-        # Open avg file and add averages
-        with open(avgfile, 'a') as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([dayToAverage, sum(temps)/len(temps), sum(hums)/len(hums), sum(dewps)/len(dewps)])
+        except (IOError, TypeError, KeyError, ZeroDivisionError):
+            successFlag = False
+
+        if successFlag:
+            os.remove(file)
 
