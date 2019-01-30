56204d3ce4cdb7811d8b71d9a5f0c38759059d38
Sander van Doorn <s.van.doorn@st.hanze.nl>
2019-01-30 11:43:25 +0100

    Create script to calculate averages of previous day Updated jsonparser from_file and client examplejson to match new format

---
+++ storageserver/cleanhistory.py
@@ -0,0 +1,46 @@
+import datetime
+import os
+import configparser
+import csv
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+rootdir = config['lacthosa']['filepath']
+
+
+# Average the previous day and delete all measurements
+
+yesterday = datetime.datetime.now() - datetime.timedelta(1)
+dayToAverage = yesterday.strftime('%d-%m-%Y')
+
+subdirs = [x[1] for x in os.walk(rootdir)]
+stationFolders = subdirs[0]
+
+# For each station directory:
+for statdir in stationFolders:
+    file = rootdir + statdir + '/' + dayToAverage + '.csv'
+    avgfile = rootdir + statdir + '/averages.csv'
+    if os.path.exists(file):
+        temps = []
+        hums = []
+        dewps = []
+
+        # Read yesterdays csv files and store all data in lists
+        with open(file) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                temps.append(float(row['temperature']))
+                hums.append(float(row['humidity']))
+                dewps.append(float(row['dewpoint']))
+
+        # Check if avg file exists or create
+        if not os.path.exists(avgfile):
+            with open(avgfile, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+                writer.writerow(['date', 'temperature', 'humidity', 'dewpoint'])
+
+        # Open avg file and add averages
+        with open(avgfile, 'a') as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([dayToAverage, sum(temps)/len(temps), sum(hums)/len(hums), sum(dewps)/len(dewps)])
+
